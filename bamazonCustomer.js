883efa3d0b7d9549198eed0b694c8fa682b507e1
David Schwartz <davidschwartz23@gmail.com>
2019-06-14 00:10:49 -0700

    add 2 questions and parse answers

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -35,10 +35,68 @@ function queryAllProducts() {
           " | " +
           res[i].stockQuantity
       );
+      console.log("-----------------------------------");
     }
-    console.log("-----------------------------------");
+    purchaseProduct();
   });
 }
+var purchaseProduct = function() {
+  inquirer
+    .prompt([
+      {
+        name: "id",
+        type: "input",
+        message: "Enter Product ID Number:"
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many items would you like to buy?"
+      }
+    ])
+    .then(function(answer) {
+      connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+
+        var chosenItem;
+        for (var i = 0; i < res.length; i++) {
+          if (res[i].id === parseInt(answer.id)) {
+            chosenItem = res[i];
+            console.log(res[i].id);
+            console.log(parseInt(answer.id));
+            console.log("Your product is " + JSON.stringify(chosenItem));
+          }
+        }
+
+        if (chosenItem.stockQuantity > parseInt(answer.quantity)) {
+          connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [
+              {
+                stockQuantity:
+                  chosenItem.stockQuantity - parseInt(answer.quantity)
+              },
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw error;
+              console.log(
+                "Your total is " +
+                  "$" +
+                  parseInt(answer.quantity) * chosenItem.price
+              );
+            }
+          );
+        } else {
+          console.log(
+            "We're sorry. We don't have enough of that product for you to purchase."
+          );
+        }
+      });
+    });
+};
 
 // function productIdSearch() {
 //   inquirer
